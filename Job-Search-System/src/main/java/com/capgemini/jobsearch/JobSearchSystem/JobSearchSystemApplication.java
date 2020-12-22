4e13117d19e62a969bbf9a552a7e011fcbd1bb46
admin <admin@DESKTOP-HO4P0CS>
2020-12-22 21:44:07 +0530

    Initial commit

---
+++ Job-Search-System/src/main/java/com/capgemini/jobsearch/JobSearchSystem/JobSearchSystemApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.jobsearch.JobSearchSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearchSystemApplication.class, args);
+	}
+
+}
---
+++ Job-Search-System/src/test/java/com/capgemini/jobsearch/JobSearchSystem/JobSearchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.jobsearch.JobSearchSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
